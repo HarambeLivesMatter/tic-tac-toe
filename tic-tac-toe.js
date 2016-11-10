430fba2eb0db52cc8363f677c2e8fcebe9e755d6
Giovanni vargas-Limon <Obliviousair22@gmail.com>
2016-11-10 15:00:02 -0800

    alert

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,3 +1,7 @@
 // Write all your JavaScript code in this file!
 // You can use the namespace variable that's defined here.
 var namespace = "http://www.w3.org/2000/svg"
+
+function square1() {
+  alert("hi")
+}
