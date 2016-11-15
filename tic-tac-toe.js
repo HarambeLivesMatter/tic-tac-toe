c96947b7b652593ae82eb291f058f855cba5e062
Giovanni vargas-Limon <Obliviousair22@gmail.com>
2016-11-15 14:34:24 -0800

    got a circle to apear on top left

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,5 +3,11 @@
 var namespace = "http://www.w3.org/2000/svg"
 
 function square1() {
-  alert("hi")
+  var canvas = document.getElementById("game-board")
+  var circle = document.createElementNS(namespace ,"circle")
+  circle.setAttribute ("cx",25)
+  circle.setAttribute ("cy",25)
+  circle.setAttribute ("r",20)
+  circle.setAttribute ("fill","red")
+  canvas.appendChild(circle)
 }
