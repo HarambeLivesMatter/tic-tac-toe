c710c4b38c2258175728f3cfabda39f0cf5a0c70
Giovanni vargas-Limon <Obliviousair22@gmail.com>
2016-11-15 15:01:38 -0800

    variable

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,13 +1,16 @@
 // Write all your JavaScript code in this file!
 // You can use the namespace variable that's defined here.
 var namespace = "http://www.w3.org/2000/svg"
-
-function square1() {
-  var canvas = document.getElementById("game-board")
-  var circle = document.createElementNS(namespace ,"circle")
-  circle.setAttribute ("cx",25)
-  circle.setAttribute ("cy",25)
-  circle.setAttribute ("r",20)
-  circle.setAttribute ("fill","red")
-  canvas.appendChild(circle)
+var topleftclicked = 0
+function topleft() {
+  if (topleftclicked == 0){
+    var canvas = document.getElementById("game-board")
+    var circle = document.createElementNS(namespace ,"circle")
+    circle.setAttribute ("cx",25)
+    circle.setAttribute ("cy",25)
+    circle.setAttribute ("r",20)
+    circle.setAttribute ("fill","red")
+    canvas.appendChild(circle)
+    topleftclicked = 1
+  }
 }
